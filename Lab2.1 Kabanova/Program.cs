e99626d73b7fab95f24e493e0e128fef1159619d
alexa <alexa@DESKTOP-RV4BEBB>
2021-10-20 22:19:40 +0200

    Добавьте файлы проекта.

---
+++ Lab2.1 Kabanova/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите x:");
+                double x = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите y:");
+                double y = double.Parse(Console.ReadLine());
+                if (Math.Pow(x + 1, 2) + y * y >= 1 && x * x + y * y <= 4) Console.WriteLine("Yes");
+                else Console.WriteLine("No");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
